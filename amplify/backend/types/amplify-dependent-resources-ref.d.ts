3062494a9852c173fec1e808d4d1ed695fa84e62
Akobir <akobirbahronov07@gmail.com>
2023-08-01 14:23:23 +0500

    Adding GraphQl Api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphqlac503bf6": {
       "AppClientID": "string",
